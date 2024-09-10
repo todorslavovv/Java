77317b27bcd0de11458a2a5cabaf4457d4aa2173
todorslavovv <todor.24.11a.2019@gmail.com>
2024-09-10 04:47:19 +0300

    Java programming journey

---
+++ Java Projects/part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Orwell {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(sc.nextLine());
+        
+
+        if (number == 1984) {
+            System.out.println("Orwell");
+        }
+
+    }
+}
