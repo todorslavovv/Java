541d91512b2b1fe0396f604cab41b7b1d53de1a4
todorslavovv <todor.24.11a.2019@gmail.com>
2024-09-12 23:56:21 +0300

    Java programming journey

---
+++ Java Projects/part02-Part02_01.Squared/src/main/java/Squared.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int number = Integer.valueOf(sc.nextLine());
+        
+        System.out.println(number * number);
+    }
+}
