2935cccafa88e9545280fe45a30996497d8a27cb
todorslavovv <todor.24.11a.2019@gmail.com>
2024-09-16 02:38:50 +0300

    Java programming journey

--- Java Projects/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ Java Projects/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -4,26 +4,14 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Read two integers from the user
-        System.out.println("Enter the first number:");
-        int firstNumber = Integer.valueOf(sc.nextLine());
-
-        System.out.println("Enter the second number:");
-        int secondNumber = Integer.valueOf(sc.nextLine());
-
-        // Calculate the sum
-        int sum = firstNumber + secondNumber;
-
-        // Check if the sum is non-negative
-        if (sum < 0) {
-            System.out.println("The sum is negative. Cannot calculate square root.");
-        } else {
-            // Calculate the square root
-            double squareRoot = Math.sqrt(sum);
-            System.out.println((int)squareRoot); // Cast to int for whole number output as in examples
-        }
-
-        sc.close();
+        
+        int num1 = Integer.valueOf(sc.nextLine());
+        int num2 = Integer.valueOf(sc.nextLine());
+    
+        int num3 = num1 + num2;
+        
+        double squareRoot = Math.sqrt(num3);
+        
+        System.out.println(squareRoot);
     }
 }
--- Java Projects/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ Java Projects/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,25 +1,20 @@
-
 import java.util.Scanner;
 
 public class AverageOfNumbers {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        double count = 0;
+        double average = 0;
         while (true) {
-
             System.out.println("Give a number:");
-
-            int num = Integer.valueOf(sc.nextLine());
-
-            if (num == 0) {
-                System.out.println("Sum of the numbers: " + sum);
-                System.out.println("Number of numbers: " + count);
+            int number = Integer.valueOf(scanner.nextLine());
+            count = count + 1;
+            average = average + number;
+            if (number == 0) {
+                count = count - 1;
+                System.out.println("Average of the numbers: " +(double)(average / count));
                 break;
-            } else if (num != 0) {
-                sum = sum + num;
-                count = count + 1;
             }
 
         }
